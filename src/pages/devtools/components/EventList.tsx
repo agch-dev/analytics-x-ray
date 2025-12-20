1e2e1178ae4cfdce896b3cdbf5f09c9c58f8375e
Agustina Chaer <aguschaer@gmail.com>
2025-12-20 19:25:07 -0300

    Update EventList component styling to improve spacing by adjusting padding and margin properties.

--- src/pages/devtools/components/EventList.tsx
+++ src/pages/devtools/components/EventList.tsx
@@ -271,7 +271,7 @@ export const EventList = forwardRef<EventListHandle, EventListProps>(
       <div
         ref={scrollContainerRef}
         onScroll={handleScroll}
-        className="flex-1 overflow-y-auto relative px-4 pt-2 pb-2"
+        className="flex-1 overflow-y-auto relative px-4 mt-2 mb-2"
       >
         {/* Sticky header overlay - clickable to collapse and scroll to event */}
         {stickyEvent && (
